bc477077847a1652956e10d59e542e8a46906377
Scott <scottbbarker@gmail.com>
2020-03-23 22:03:35 +0000

    upload

--- Registry/Graham.ps1
+++ Registry/Graham.ps1
@@ -2,40 +2,40 @@
 $serverdetails = @()
 
 #Get all the server names from the servers.txt file.
-$servers = Get-Content -Path C:\Users\csweeny3\Desktop\servers.txt
+$servers = Get-Content -Path .\servers.txt
 
 foreach ($server in $servers) {
-#check remote registry service is running
-$service = Get-Service -ComputerName $server -Name RemoteRegistry -ErrorAction SilentlyContinue
-$test = $service.Status
+    #check remote registry service is running
+    $service = Get-Service -ComputerName $server -Name RemoteRegistry -ErrorAction SilentlyContinue
+    $test = $service.Status
 
-if ($service.status -eq 'Running') {
-    $type = [Microsoft.Win32.RegistryHive]::LocalMachine
-    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($type, $server)
-    #Make sure our keys are null every time
-    $KeyV3 = $null
-    $KeyV4 = $null
-    #Check for version 3 serial    
-    $keyV3 = $reg.OpenSubKey("SOFTWARE\triCerat\Simplify Printing\ScrewDrivers Server v3")
-    #Check for version 4 serial
-    $keyV4 = $reg.OpenSubKey("SOFTWARE\triCerat\Simplify Printing\ScrewDrivers Server v4")
-    #Check if there is a V3 Serial key and create an object if there is
-    if ($keyV3 -ne $null) {
-    $object = [pscustomobject][ordered]@{"Server Name"=$server;"Service Status"=$service.status;"V3 Licence"=$keyV3.GetValue("licenseSerial");"V4 Licence"=$null}
-    $serverdetails += $object
+    if ($service.status -eq 'Running') {
+        $type = [Microsoft.Win32.RegistryHive]::LocalMachine
+        $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($type, $server)
+        #Make sure our keys are null every time
+        $KeyV3 = $null
+        $KeyV4 = $null
+        #Check for version 3 serial    
+        $keyV3 = $reg.OpenSubKey("SOFTWARE\triCerat\Simplify Printing\ScrewDrivers Server v3")
+        #Check for version 4 serial
+        $keyV4 = $reg.OpenSubKey("SOFTWARE\triCerat\Simplify Printing\ScrewDrivers Server v4")
+        #Check if there is a V3 Serial key and create an object if there is
+        if ($keyV3 -ne $null) {
+            $object = [pscustomobject][ordered]@{"Server Name" = $server; "Service Status" = $service.status; "V3 Licence" = $keyV3.GetValue("licenseSerial"); "V4 Licence" = $null }
+            $serverdetails += $object
+        }
+        #Check if there is a V4 Serial Key and create an object if there is
+        elseif ($keyV4 -ne $null) {
+            $object = [pscustomobject][ordered]@{"Server Name" = $server; "Service Status" = $service.status; "V3 Licence" = $null; "V4 Licence" = $keyV4.GetValue("licenseSerial") }
+            $serverdetails += $object
+        }
     }
-    #Check if there is a V4 Serial Key and create an object if there is
-    elseif ($keyV4 -ne $null) {
-    $object = [pscustomobject][ordered]@{"Server Name"=$server;"Service Status"=$service.status;"V3 Licence"=$null;"V4 Licence"=$keyV4.GetValue("licenseSerial")}
-    $serverdetails += $object
-    }
-}
-else {
-    #Could put some code in here to try and turn the remote registry service on?    
-    $object = [pscustomobject][ordered]@{"Server Name"=$server;"Service Status"="Stopped";"V3 Licence"=$null;"V4 Licence"=$null}
-    $serverdetails += $object
+    else {
+        #Could put some code in here to try and turn the remote registry service on?    
+        $object = [pscustomobject][ordered]@{"Server Name" = $server; "Service Status" = "Stopped"; "V3 Licence" = $null; "V4 Licence" = $null }
+        $serverdetails += $object
     }
 }
 
 #Ouput the information that we have gathered.
-$serverdetails | ConvertTo-Html | Out-file C:\Users\csweeny3\Desktop\results.html
+$serverdetails | ConvertTo-Html | Out-file .\results.html
--- VMware/Provisioned/psProvisioned.ps1
+++ VMware/Provisioned/psProvisioned.ps1
@@ -1,30 +1,32 @@
-$vcenter = "BASXTSPRDVCW01"
-$strCluster = "Basildon Internal 01"
-$strFolderPath = "C:\Temp\Sbarker\Scripts\Vmware"
+$vcenter = "vcentername"
+$strCluster = "cluster name"
+$strFolderPath = ".\Vmware"
 
 
 # Adds the powercli cmdlets
 Add-PSSnapin VMware.VimAutomation.Core -erroraction SilentlyContinue
 
 # Disconnects from any viserver to stop potential false results in ouput
-if ($global:DefaultVIServer -ne $null){
-    write-host "Disconnecting from current vcenter(s)"
-	disconnect-viserver * -confirm:$false}
-	else
-	{write-host "Not connected to any vCenter, continuing"}
+if ($global:DefaultVIServer -ne $null) {
+	write-host "Disconnecting from current vcenter(s)"
+	disconnect-viserver * -confirm:$false
+}
+else
+{ write-host "Not connected to any vCenter, continuing" }
 
 Connect-VIServer -Server $vcenter
 
 #Get host CPU and Memory
 Get-Cluster $strCluster | Get-VMHost | Sort Name | `
-Get-View | Select Name,Version, build, @{N="Type";E={$_.Hardware.SystemInfo.Vendor+ " " + $_.Hardware.SystemInfo.Model}},`
-@{N="CPU";E={"PROC:" + $_.Hardware.CpuInfo.NumCpuPackages + " CORES:" + $_.Hardware.CpuInfo.NumCpuCores + " MHZ: " + `
-[math]::round($_.Hardware.CpuInfo.Hz / 1000000, 0)}},`
-@{N="MEM";E={"" + [math]::round($_.Hardware.MemorySize / 1GB, 0) + " GB"}} `
+	Get-View | Select Name, Version, build, @{N = "Type"; E = { $_.Hardware.SystemInfo.Vendor + " " + $_.Hardware.SystemInfo.Model } }, `
+@{N = "CPU"; E = { "PROC:" + $_.Hardware.CpuInfo.NumCpuPackages + " CORES:" + $_.Hardware.CpuInfo.NumCpuCores + " MHZ: " + `
+			[math]::round($_.Hardware.CpuInfo.Hz / 1000000, 0) }
+}, `
+@{N = "MEM"; E = { "" + [math]::round($_.Hardware.MemorySize / 1GB, 0) + " GB" } } `
 | Export-Csv "$strFolderPath\$strCluster.csv" -NoType
 
 #List assigned CPU & Memory to VMs in the VMware cluster
 Get-cluster $strCluster | Get-VM | Select Name, NumCPU, MemoryGB | Export-Csv "$strFolderPath\$strCluster-VMS.csv" -NoType
 
 #List Datastore per Cluster
-Get-Cluster $strCluster | Get-VMHost | Get-Datastore | Select Name,CapacityGB,FreeSpaceGB | Export-CSV "$strFolderPath\$strCluster-DS.csv" -Notype
+Get-Cluster $strCluster | Get-VMHost | Get-Datastore | Select Name, CapacityGB, FreeSpaceGB | Export-CSV "$strFolderPath\$strCluster-DS.csv" -Notype
