27ee942d97f56131aa92986243e7c1780ccfd7fc
Barker <sbarker23@dxc.com>
2020-09-23 16:47:50 +0100

    uploading docs

--- VMware/Snapshot/psDeleteSnapshotsPatching.ps1
+++ VMware/Snapshot/psDeleteSnapshotsPatching.ps1
@@ -1,4 +1,33 @@
-﻿$vCenterServer = "vcname"
+﻿try
+{ $vCenterConnection = Connect-VIServer $ComputerName -WarningVariable vCenterConnectWarnings -ErrorVariable vCenterConnectErrors }
+catch
+{        
+    Write-Verbose "$([Datetime]::Now.ToShortDateString()) $([Datetime]::Now.ToString("HH:mm:ss")) Connection failed"
+    # If the connection fails report a failure with Connet-VIServer
+    $vCenterResponse.Component = "Connectivity"
+    $vCenterResponse.Message = "Failed to connect to vCenter"
+    $vCenterResponse.Detail = "Failed to connect to vCenter, error message: $($_.Exception.Message)"
+    return $vCenterResponse
+}
+$ErrorActionPreference = $OriginalErrorActionPreference
+
+# If the connection fails report a failure with Connet-VIServer
+if(($vCenterConnection -and $vCenterConnection.IsConnected -eq $false) -or !$vCenterConnection)
+{
+    Write-Verbose "$([Datetime]::Now.ToShortDateString()) $([Datetime]::Now.ToString("HH:mm:ss")) Connection failed"
+    $vCenterResponse.Component = "Connectivity"
+    $vCenterResponse.Message = "Failed to connect to vCenter"
+    if($vCenterConnectErrors)
+    { $vCenterResponse.Detail = "Failed to connect to vCenter, error message: $($vCenterConnectErrors[0].Exception.Message)" }
+    else
+    { $vCenterResponse.Detail = "Failed to connect to vCenter" }
+    return $vCenterResponse
+}
+
+
+
+
+$vCenterServer = "vcname"
 $Snapshotdays =""
 
 Add-PSSnapin VMware.VimAutomation.Core -ErrorAction SilentlyContinue 
@@ -21,6 +50,30 @@ if($VMs -and $VMs.Count -gt 0)
                 
                 }
             
-}
+        }
 
 Disconnect-VIServer -Server $vCenter -Confirm:$false
+
+
+#Connect to vSphere
+$VIServer = Read-Host "Enter IP or Hostname for your VI Server"
+$VIUser = Read-Host "Enter Username"
+$VIPass = Read-Host "Enter Password"
+Connect-VIServer -server $VIServer -user $VIUser -pass $VIPass
+
+#Enumerate VMs and search for Protect Snapshots.  Red machines have snapshots to remove, green have no snapshots.
+foreach ($vm in get-vm | sort Name) {
+                $vmname = $vm.name
+                $snaps = get-snapshot -vm $vm 
+                foreach ($snap in $snaps) {
+                                $snapName = $snap.name
+                                if ($snapname -like "Protect Patch") {
+                                                Write-Host "Found snapshot: $snapname on $vmname" -foregroundcolor red 
+                                               
+                                                remove-snapshot -snapshot $snap -confirm:$false
+                                }
+                                Else {
+                                                Write-Host "No protect snapshot found on $vmname" -foregroundcolor green 
+                                }
+                }
+}
