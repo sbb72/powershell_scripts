138da7bb30cda9c0f47925f2d59f3413b134532a
Scott <scottbbarker@gmail.com>
2020-09-23 22:13:02 +0100

    added to script

--- VMware/Snapshot/psDeleteSnapshotsPatching.ps1
+++ VMware/Snapshot/psDeleteSnapshotsPatching.ps1
@@ -1,61 +1,62 @@
-﻿Get-VM server | Get-Snapshot | Where {$_.name -like "#Patching#*"} | Remove-Shapshot
-
-
-
-
-try
-{ $vCenterConnection = Connect-VIServer $ComputerName -WarningVariable vCenterConnectWarnings -ErrorVariable vCenterConnectErrors }
-catch
-{        
-    Write-Verbose "$([Datetime]::Now.ToShortDateString()) $([Datetime]::Now.ToString("HH:mm:ss")) Connection failed"
-    # If the connection fails report a failure with Connet-VIServer
-    $vCenterResponse.Component = "Connectivity"
-    $vCenterResponse.Message = "Failed to connect to vCenter"
-    $vCenterResponse.Detail = "Failed to connect to vCenter, error message: $($_.Exception.Message)"
-    return $vCenterResponse
-}
-$ErrorActionPreference = $OriginalErrorActionPreference
-
-# If the connection fails report a failure with Connet-VIServer
-if(($vCenterConnection -and $vCenterConnection.IsConnected -eq $false) -or !$vCenterConnection)
-{
-    Write-Verbose "$([Datetime]::Now.ToShortDateString()) $([Datetime]::Now.ToString("HH:mm:ss")) Connection failed"
-    $vCenterResponse.Component = "Connectivity"
-    $vCenterResponse.Message = "Failed to connect to vCenter"
-    if($vCenterConnectErrors)
-    { $vCenterResponse.Detail = "Failed to connect to vCenter, error message: $($vCenterConnectErrors[0].Exception.Message)" }
-    else
-    { $vCenterResponse.Detail = "Failed to connect to vCenter" }
-    return $vCenterResponse
+﻿$vCenterServer = "vcname"
+$Snapshotdays = "-356"
+$PatchingDate = Get-date -f dd-MM-yyyy
+function DeleteSnapshots {
+    param (
+        OptionalParameters
+    )
+    
+    Get-VM | Get-Snapshot | Where { $_.name -like "#Patching#*" -and $_.created -lt (get-date).addDays(-7) } | Remove-Shapshot -confirm:$false -whatif
 }
 
 
 
+function CreateSnapshot {
+    param (
+        [Switch]$ServerList,
+        [Switch]$Server
+    )
+    ForEach ($Server in $ServerList) {
+    
+        $NewSnapshot = New-Snapshot -VM $Server -Name "#Patching# on $PatchingDate" -Description "Created by $env:USERNAME 24/7 Team $PatchingDate"
+        If ($NewSnapshot) {
+            Return "Snapshot $Snapshot created."
+        }
+        else {
+            Return "Error occured during snapshot creation."
+        }
+        New-Snapshot -Name "#Patching# at $PatchingDate" -Description "Created by $env:USERNAME 24/7 Team $PatchingDate"
+  
+    }  
+}
 
-$vCenterServer = "vcname"
-$Snapshotdays =""
+
 
 Add-PSSnapin VMware.VimAutomation.Core -ErrorAction SilentlyContinue 
 
 Connect-VIServer $vCenterServer
 
-$VMs = Get-VM | Select Name
+try
+{ $vCenterConnection = Connect-VIServer $ComputerName -WarningVariable vCenterConnectWarnings -ErrorVariable vCenterConnectErrors }
+catch {        
+    Write-Verbose "Failed to connect to vCenter, error message: $($_.Exception.Message)"
+    $vCenterResponse.Detail = "Failed to connect to vCenter, error message: $($_.Exception.Message)"
+}
 
-if($VMs -and $VMs.Count -gt 0)
-        {
-            # Check for any VM snapshots            
-            [Array]$Snapshots = Get-View -ViewType VirtualMachine -Filter @{"Snapshot"=""} -Server $vCenterServer
-                foreach($Snapshot in $Snapshots)
-                {
-                    if($Snapshot.Snapshot.RootSnapshotList.CreateTime -lt (Get-Date).AddDays(-356))
-                    {
-                        Write-Host "Deleting $($Snapshot.Snapshot.RootSnapshotList.Name) for $($Snapshot.Name), created on $($Snapshot.Snapshot.RootSnapshotList.CreateTime), current size $([Math]::Round(($Snapshot.LayoutEx.File | Where-Object { $_.Name -match "-delta.vmdk" } | Measure-Object -Property Size -Sum).Sum/1MB,0))MB and power state $($Snapshot.Runtime.PowerState)"
-                        get-vm -Name $($Snapshot.Name) | Get-Snapshot | Remove-Snapshot -Confirm:$false
-                    }
-                
-                }
-            
-        }
+
+$VMs = Get-VM | Select-Object Name
+
+# Check for any VM snapshots            
+[Array]$Snapshots = Get-View -ViewType VirtualMachine -Filter @{"Snapshot" = "" } -Server $vCenterServer
+foreach ($Snapshot in $Snapshots) {
+    if ($Snapshot.Snapshot.RootSnapshotList.CreateTime -lt (Get-Date).AddDays($Snapshotdays) -and ) {
+        Write-Host "Deleting $($Snapshot.Snapshot.RootSnapshotList.Name) for $($Snapshot.Name), created on $($Snapshot.Snapshot.RootSnapshotList.CreateTime), current size $([Math]::Round(($Snapshot.LayoutEx.File | Where-Object { $_.Name -match "-delta.vmdk" } | Measure-Object -Property Size -Sum).Sum/1MB,0))MB and power state $($Snapshot.Runtime.PowerState)"
+        get-vm -Name $($Snapshot.Name) | Get-Snapshot | Remove-Snapshot -Confirm:$false
+    }
+    
+}
+
+
 
 Disconnect-VIServer -Server $vCenter -Confirm:$false
 
@@ -68,17 +69,17 @@ Connect-VIServer -server $VIServer -user $VIUser -pass $VIPass
 
 #Enumerate VMs and search for Protect Snapshots.  Red machines have snapshots to remove, green have no snapshots.
 foreach ($vm in get-vm | sort Name) {
-                $vmname = $vm.name
-                $snaps = get-snapshot -vm $vm 
-                foreach ($snap in $snaps) {
-                                $snapName = $snap.name
-                                if ($snapname -like "Protect Patch") {
-                                                Write-Host "Found snapshot: $snapname on $vmname" -foregroundcolor red 
+    $vmname = $vm.name
+    $snaps = get-snapshot -vm $vm 
+    foreach ($snap in $snaps) {
+        $snapName = $snap.name
+        if ($snapname -like "Protect Patch") {
+            Write-Host "Found snapshot: $snapname on $vmname" -foregroundcolor red 
                                                
-                                                remove-snapshot -snapshot $snap -confirm:$false
-                                }
-                                Else {
-                                                Write-Host "No protect snapshot found on $vmname" -foregroundcolor green 
-                                }
-                }
+            remove-snapshot -snapshot $snap -confirm:$false
+        }
+        Else {
+            Write-Host "No protect snapshot found on $vmname" -foregroundcolor green 
+        }
+    }
 }
