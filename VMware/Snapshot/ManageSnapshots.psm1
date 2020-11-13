b667880b8641a09ca6cce5ef3bebc25e7b57f3be
Barker <sbarker23@dxc.com>
2020-11-13 15:44:56 +0000

    added deletesnapshots

--- VMware/Snapshot/ManageSnapshots.psm1
+++ VMware/Snapshot/ManageSnapshots.psm1
@@ -4,7 +4,7 @@
         [switch]$DeleteSnapshot = $null,
         [Switch]$ServerList,
         [string]$User = $env:USERNAME,
-
+        [switch]$Snapshotdetails = "$env:USERNAME-Created Snapshot",
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [Switch]$vCenterServer,
@@ -32,3 +32,36 @@
         
     }  
 }
+
+
+<#
+function deletesnapshots {
+    param (
+      
+    )
+    $AllNamedSnapshots = Get-cluster NonProductionCluster | Get-Snapshot | Where Name -eq "PreToolsandHardware20180310"
+    $SnapshotRemaining = $AllNamedSnapshots.count
+    $Tasks = @{}
+     
+    While ($SnapshotRemaining -gt 0){
+        $RemoveSnapshotTasks = $AllNamedSnapshots | Select -First 15
+        foreach($SnapShottobeRemoved in $RemoveSnapshotTasks){
+            $Tasks[(Remove-Snapshot $SnapShottobeRemoved -RunAsync -Confirm:$false).id] = $SnapShottobeRemoved
+        }
+        $RunningTasks = $Tasks.count
+        While ($RunningTasks -gt 0){
+            Get-Task | % {
+                If($tasks.containskey($_.id) -and $_.state -eq "Success"){
+                    $tasks.Remove($_.Id)
+                    $RunningTasks--
+                    $SnapshotRemaining--
+                }
+            }
+            Start-Sleep -Seconds 10
+            Write-host "There are still $RunningTasks task(s) running" -BackgroundColor Black -ForegroundColor Yellow
+        }
+        $AllNamedSnapshots = Get-cluster NonProductionCluster | Get-Snapshot | Where Name -eq "PreToolsandHardware20180310"
+    }
+}
+
+>#
