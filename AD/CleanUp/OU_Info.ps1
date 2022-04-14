0c13efb103d2206d624f6ccfa8946625a62f6dad
sbarker23 <sbarker23@dxc.com>
2022-04-14 10:33:07 +0100

    Upload after testing

--- AD/CleanUp/OU_Info.ps1
+++ AD/CleanUp/OU_Info.ps1
@@ -1,33 +1,33 @@
 ﻿$OUs = Get-ADOrganizationalUnit -Filter * -Properties CanonicalName,Name,DistinguishedName, Created, Description, whenChanged, gplink | Select CanonicalName,Name,DistinguishedName, Created, Description, whenChanged, gplink
+$LogFile = "Path\OUCleanup_"+$($(Get-Date -Format dd-MM-yyy))+".csv"
 
-$LogFile = "F:\SBarker\Clean-up\"+(Get-Date -Format dd_MM_yyy)+"_OUInfo.csv"
-$FullResults =@()
+$FullResults = New-Object System.Collections.ArrayList
 
 ForEach ($OU in $OUs) {
+    
+    Write-host "Getting data for $($OU.Name)" -ForegroundColor Green
 
-If ("$($OU.gplink)") {$GPlink = "YES"} ELSE {$GPlink = "NO"}
+    If ("$($OU.gplink)") {$GPlink = "YES"} ELSE {$GPlink = "NO"}
+          
+        $Results = [pscustomobject]@{
 
-    $Results = [pscustomobject]@{
+            Name = $OU.Name
+            CanonicalName = $OU.CanonicalName
+            DistinguishedName = $OU.DistinguishedName
+            Created = $OU.Created
+            Description = $OU.Description
+            whenChanged = $OU.whenChanged
+            UserCount = (Get-Aduser -Filter * -SearchBase $OU.DistinguishedName -SearchScope OneLevel | Measure-Object).count
+            ComputerCount = (Get-ADComputer -Filter * -SearchBase $OU.DistinguishedName -SearchScope OneLevel | Measure-Object).Count
+            GroupCount = (Get-AdGroup -filter * -searchbase $OU.DistinguishedName | Measure-Object).Count
+            GPOLinked = $GPlink
+        
+        }
 
-    Name = $OU.Name
-    CanonicalName = $OU.CanonicalName
-    DistinguishedName = $OU.DistinguishedName
-    Created = $OU.Created
-    Description = $OU.Description
-    whenChanged = $OU.whenChanged
-    UserCount = (Get-Aduser -Filter * -SearchBase $OU.DistinguishedName -SearchScope OneLevel | Measure-Object).count
-    ComputerCount = (Get-ADComputer -Filter * -SearchBase $OU.DistinguishedName -SearchScope OneLevel | Measure-Object).Count
-    GroupCount = (Get-AdGroup -filter * -searchbase $OU.DistinguishedName | Measure-Object).Count
-    GPOLinked = $GPlink
-
-    }
-
+ 
 $FullResults += $Results
 
-#$OU.gplink = ""
-
 Remove-Variable -Name GPlink
-}
+} 
 
-
-$FullResults | Export-Csv -Path $LogFile -NoTypeInformation
+$FullResults | Export-Csv -Path $Logfile -NoTypeInformation
