0e99b00fcc4c6ffc891c225c7cff9b216d41d97c
sbarker23 <sbarker23@dxc.com>
2024-03-15 17:47:07 +0000

    Added more date, tee and format

--- Golf_stuff/Getcsvfile.ps1
+++ Golf_stuff/Getcsvfile.ps1
@@ -2,30 +2,27 @@ $foldername = "C:\Users\Scott\Downloads\Wood_Bowl_(Major)_NettScoreSheet_15_03_2
 
 $csvdata = Import-csv -Path $foldername 
 
+#$csvTeeBox | Select-Object Filename
+
+#$csvdata | Where-Object {$_.'Date:' -match $searchTerm -or $_.system -match $searchTerm} |Select-Object -Expand Name
 
 
-
-
-
-$csvTeeBox | Select-Object Filename
-
-$csvdata | Where-Object {$_.'Date:' -match $searchTerm -or $_.system -match $searchTerm} |Select-Object -Expand Name
-
-
-$Test = $csvDate.ToString().Split(';') | Select-String Name
+#$Test = $csvDate.ToString().Split(';') | Select-String Name
 
 #Get date of comp from csv file
 $csvDate = $csvdata | select-string "Date:" 
-$Test = $csvDate.ToString().Split(';') | Select-String Name
-$Test.ToString("dd-MM-yyyy").Split('=')[1]
-Get-Date $Test.ToString("dd-MM-yyyy").Split('=')[1] -Format 'dd-MM-yyyy'
+$csvDate = $csvDate.ToString().Split(';') | Select-String Name
+$csvDate.ToString("dd-MM-yyyy").Split('=')[1]
+$csvDate = Get-Date $Test.ToString("dd-MM-yyyy").Split('=')[1] -Format 'dd-MM-yyyy'
 
 #Format of play
 $csvGameFormat = $csvdata | select-string 'Score Type:'
-$csvGameFormat  = $csvGameType.ToString().Split(';') | Select-String Name
+$csvGameFormat  = $csvGameFormat.ToString().Split(';') | Select-String Name
 $csvGameFormat = $csvGameFormat.ToString().Split('=')[1]
 
 #Tee
 $csvTeeBox = $csvdata | select-string 'Course/Tee:'
 $csvTeeBox  = $csvTeeBox.ToString().Split(';') | Select-String Name
-$csvTeeBox = $csvTeeBox.ToString().Split('=')[1]
+$csvTeeBox = ($csvTeeBox.ToString().Split('=')[1]).TrimEnd()
+
+Write-Output "Competition date was '$csvDate', played on '$csvTeeBox' tees and format was '$csvGameFormat'"
