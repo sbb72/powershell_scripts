36bd9c1a3cc6baa97b2a7f6976125789bc074907
sbarker23 <sbarker23@dxc.com>
2024-03-15 19:52:08 +0000

    updated working script

--- Golf_stuff/Getcsvfile.ps1
+++ Golf_stuff/Getcsvfile.ps1
@@ -9,20 +9,49 @@ $csvdata = Import-csv -Path $foldername
 
 #$Test = $csvDate.ToString().Split(';') | Select-String Name
 
-#Get date of comp from csv file
-$csvDate = $csvdata | select-string "Date:" 
-$csvDate = $csvDate.ToString().Split(';') | Select-String Name
-$csvDate.ToString("dd-MM-yyyy").Split('=')[1]
-$csvDate = Get-Date $Test.ToString("dd-MM-yyyy").Split('=')[1] -Format 'dd-MM-yyyy'
+$CompDataResponse  = New-Object System.Collections.ArrayList
+$Response  = New-Object System.Collections.ArrayList
+Foreach ($csvline in $csvdata) 
+{
+    $CompDataResponse = New-Object -TypeName PSObject -Property @{
+        "@timestamp" = [datetime]::UtcNow;
+        "CompetitionDate" = "";
+        "Format" = "";
+        "TeeBox" = "";
+        "GolfersName" = "";
+        "Position" = "";
+        "Handicap" = "";
+        "ScorceGross" ="";
+        "ScoreNet" = "";
+    }
 
-#Format of play
-$csvGameFormat = $csvdata | select-string 'Score Type:'
-$csvGameFormat  = $csvGameFormat.ToString().Split(';') | Select-String Name
-$csvGameFormat = $csvGameFormat.ToString().Split('=')[1]
+    If ($csvline.Pos -match '^\d+$')
+    {
+        $CompDataResponse.GolfersName = $csvline.Name 
+        $CompDataResponse.Position = $csvline.Pos   
+        $CompDataResponse.ScorceGross = $csvline.Gross
+        $CompDataResponse.ScoreNet = $csvline.Nett
+        $CompDataResponse.Handicap = $csvline.Hcp
+        
+    }
+    #Get date of comp from csv file
+    $csvDate = $csvdata | select-string "Date:" 
+    $csvDate = $csvDate.ToString().Split(';') | Select-String Name
+    #$csvDate = $csvDate.ToString("dd-MM-yyyy").Split('=')[1]
+    $csvDate = Get-Date $csvDate.ToString("dd-MM-yyyy").Split('=')[1] -Format 'dd-MM-yyyy'
+    $CompDataResponse.CompetitionDate = $csvDate
+    #Format of play
+    $csvGameFormat = $csvdata | select-string 'Score Type:'
+    $csvGameFormat  = $csvGameFormat.ToString().Split(';') | Select-String Name
+    $csvGameFormat = $csvGameFormat.ToString().Split('=')[1]
+    $CompDataResponse.Format = $csvGameFormat
+    #Tee
+    $csvTeeBox = $csvdata | select-string 'Course/Tee:'
+    $csvTeeBox  = $csvTeeBox.ToString().Split(';') | Select-String Name
+    $csvTeeBox = ($csvTeeBox.ToString().Split('=')[1]).TrimEnd()
+    $CompDataResponse.TeeBox = $csvTeeBox
+    Write-Output "Competition date was '$csvDate', played on '$csvTeeBox' tees and format was '$csvGameFormat'"
 
-#Tee
-$csvTeeBox = $csvdata | select-string 'Course/Tee:'
-$csvTeeBox  = $csvTeeBox.ToString().Split(';') | Select-String Name
-$csvTeeBox = ($csvTeeBox.ToString().Split('=')[1]).TrimEnd()
-
-Write-Output "Competition date was '$csvDate', played on '$csvTeeBox' tees and format was '$csvGameFormat'"
+    $Response.Add($CompDataResponse)
+}
+Return $Response
