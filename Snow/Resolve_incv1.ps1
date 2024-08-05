acb6570232d1875ecf9bd70219c72c613b54b350
Barker <sbarker23@dxc.com>
2024-08-05 09:52:47 +0100

    added wsus script

--- Snow/Resolve_incv1.ps1
+++ Snow/Resolve_incv1.ps1
@@ -4,15 +4,17 @@
 # Yasin Kara (DXC) (August 2021)
 
 # Import module
-Import-Module DXC-iSolve -Function Update-SnowIncident
+#Import-Module DXC-iSolve -Function Update-SnowIncident
 
 # Define variables
-$SnowUrl = "cscqa.service-now.com"
+$SnowUrl = "csc.service-now.com"
 $ClosureCode = "Solved (Permanently)"
-$ClosureNotesMsg = "xRBA Testing"
+$ClosureNotesMsg = "Duplicate Ticket"
+
+Import-Module C:\Data\git-repo_work\iSolve-Powershell-Module\DXC-iSolve -Force
 
 # Path to text file containing list of incidents refs, looks for txt file alongside .ps1 file
-$Incidents = Get-Content D:\Data\git-repo\powershell-scripts\Snow\inc_numbers.txt
+$Incidents = Get-Content C:\Data\git-repo\powershell_scripts\Snow\inc_numbers.txt
 
 # Count the total number of incidents in the list
 $Total = $Incidents.Count
@@ -29,10 +31,19 @@ foreach ($INC in $Incidents)
 
     Write-Host "Trying to close $INC"
     # Make the API call to update incident
-    Get-SnowIncident -Url $SnowUrl -Number 'INC6712715' -CredentialFor 'Snow-Set-csc.service-now.com' -Verbose
-    
+    #Get-SnowIncident -Url $SnowUrl -Number $INC -CredentialFor 'Snow-Set-csc.service-now.com' -Verbose
+     
+    # Make the API call to update incident. Keep -ReturnStatus for quicker performance
     Update-SnowIncident -Url $SnowUrl -Number $INC -Resolve -CloseCode $ClosureCode -CloseNotes $ClosureNotesMsg -ReturnStatus -Verbose -ErrorAction SilentlyContinue
-
+ 
     # Output to console the progress counter
-    Write-Output "$Counter incident(s) updated from a total of $Total"
+    Write-Host "$Counter incident(s) updated from a total of $Total" -ForegroundColor DarkGreen -BackgroundColor White
 }
+
+
+#Write-SavedCredential -For Snow-Set-csc.service-now.com
+
+#$Creds = Read-SavedCredential -For Snow-Set-csc.service-now.com
+
+#$Creds.GetNetworkCredential().Password
+
